6881145bc968797243bf32b833551c2e095d28b4
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-11-02 12:35:21 +0000

    Remove extra line

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -18,7 +18,6 @@
                     @enderror
                 </div>
 
-
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="username">
                         Username
