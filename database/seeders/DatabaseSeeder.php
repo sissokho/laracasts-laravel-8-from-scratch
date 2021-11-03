91cb2ec3ac63b09fee44f20837e7b4097e15a1da
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-11-03 18:18:33 +0000

    Create fake comments for seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -17,7 +18,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $user = User::factory()->create([
-            'name' => 'Moustapha Sissokho'
+            'name' => 'Moustapha Sissokho',
+            'username' => 'sisko',
+            'email' => 'siskomouhamed@gmail.com',
+            'password' => 'password'
         ]);
 
         Category::factory()
@@ -28,6 +32,16 @@ class DatabaseSeeder extends Seeder
                     ->state(function () use ($user) {
                         return ['user_id' => $user->id];
                     })
+                    ->has(
+                        Comment::factory()
+                            ->count(3)
+                            ->state(function (array $attributes, Post $post) use ($user) {
+                                return [
+                                    'user_id' => $user->id,
+                                    'post_id' => $post->id
+                                ];
+                            })
+                    )
             )
             ->create();
     }
