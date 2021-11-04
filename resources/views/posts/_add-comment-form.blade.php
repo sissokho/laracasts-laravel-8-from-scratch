c1d90544982674053fc91008e6e0ad41af54dd49
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-11-04 14:39:40 +0000

    Make some cleanup

---
+++ resources/views/posts/_add-comment-form.blade.php
@@ -0,0 +1,27 @@
+@auth
+    <x-panel>
+        <form action="/posts/{{ $post->slug }}/comments" method="POST">
+            @csrf
+            <header class="flex items-center">
+                <img src="https://i.pravatar.cc/100?u={{ auth()->id() }}" alt="" width="40" height="40" class="rounded-full ">
+                <h2 class="ml-4">Want to participate?</h2>
+            </header>
+
+            <div class="mt-6">
+                <textarea name="body"rows="5" class="w-full text-sm focus:outline-none focus:ring" placeholder="Quick, think of something to say!" required></textarea>
+
+                @error('body')
+                    <span class="text-xs text-red-500">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
+                <x-submit-button>Post</x-submit-button>
+            </div>
+        </form>
+    </x-panel>
+@else
+    <div class="font-semibold">
+        <a href="/register" class="hover:underline">Register</a> or <a href="/login" class="hover:underline">Log in</a> to leave a comment.
+    </div>
+@endauth
