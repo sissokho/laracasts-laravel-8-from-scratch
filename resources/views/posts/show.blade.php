4d43453f708684f48923bbeac38cb12590e95ee9
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-11-04 11:55:56 +0000

    Make the comments section dynamic

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -50,7 +50,9 @@
                 </div>
 
                 <section class="col-start-5 col-span-8 mt-10 space-y-6">
-                    <x-post-comment />
+                    @foreach ($post->comments as $comment)
+                        <x-post-comment :comment="$comment" />
+                    @endforeach
                 </section>
             </article>
         </main>
