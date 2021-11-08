18af63945f61ab0fff231f854e5c1f3e5634298e
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-11-08 16:00:15 +0000

    Create and extend the settings layout

---
+++ resources/views/components/setting.blade.php
@@ -0,0 +1,25 @@
+@props(['heading'])
+
+<section class="px-6 py-8 max-w-4xl mx-auto">
+    <h1 class="text-lg font-bold mb-8 pb-2 border-b">
+        {{ $heading }}
+    </h1>
+
+    <div class="flex">
+        <aside class="w-48">
+            <h4 class="font-semibold mb-4">Links</h4>
+            <ul>
+                <li>
+                    <a href="/admin/posts/create" class="{{ request()->is('admin/posts/create') ? 'text-blue-500' : '' }}">New Post</a>
+                </li>
+            </ul>
+        </aside>
+
+        <div class="flex-1">
+            <x-panel class="max-w-sm mx-auto">
+                {{ $slot }}
+            </x-panel>
+        </div>
+    </div>
+
+</section>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,36 +1,30 @@
 <x-layout>
-    <section class="px-6 py-8">
-        <h1 class="text-lg font-bold mb-4 text-center">
-            Publish New Post
-        </h1>
+    <x-setting heading="Publish New Post">
+        <form action="/admin/posts" method="POST" enctype="multipart/form-data">
+            @csrf
+            <x-form.input name="title" />
+            <x-form.input name="slug" />
+            <x-form.input name="thumbnail" type="file" />
+            <x-form.textarea name="excerpt" />
+            <x-form.textarea name="body" />
 
-        <x-panel class="max-w-sm mx-auto">
-            <form action="/admin/posts" method="POST" enctype="multipart/form-data">
-                @csrf
-                <x-form.input name="title" />
-                <x-form.input name="slug" />
-                <x-form.input name="thumbnail" type="file" />
-                <x-form.textarea name="excerpt" />
-                <x-form.textarea name="body" />
+            <x-form.field>
+                <x-form.label name="category" />
 
-                <x-form.field>
-                    <x-form.label name="category" />
+                <select name="category_id" id="category_id">
+                    @php
+                        $categories = \App\Models\Category::all();
+                    @endphp
 
-                    <select name="category_id" id="category_id">
-                        @php
-                            $categories = \App\Models\Category::all();
-                        @endphp
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}" {{ (int) old('category_id') === $category->id ? 'selected' : '' }}>{{ ucwords($category->name) }}</option>
+                    @endforeach
+                </select>
 
-                        @foreach ($categories as $category)
-                            <option value="{{ $category->id }}" {{ (int) old('category_id') === $category->id ? 'selected' : '' }}>{{ ucwords($category->name) }}</option>
-                        @endforeach
-                    </select>
+                <x-form.error name="category" />
+            </x-form.field>
 
-                    <x-form.error name="category" />
-                </x-form.field>
-
-                <x-form.button>Publish</x-form.button>
-            </form>
-        </x-panel>
-    </section>
+            <x-form.button>Publish</x-form.button>
+        </form>
+    </x-setting>
 </x-layout>
