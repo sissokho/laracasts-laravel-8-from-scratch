b907d688ff29aa80e573bf0221a235719559f5a3
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-11-09 10:33:19 +0000

    Move create and store methods from post to admincontroller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        return view('admin.posts.index', [
+            'posts' => Post::paginate(50)
+        ]);
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'title' => ['required'],
+            'thumbnail' => ['required', 'image'],
+            'slug' => ['required', Rule::unique('posts')],
+            'excerpt' => ['required'],
+            'body' => ['required'],
+            'category_id' => ['required', Rule::exists('categories', 'id')]
+        ]);
+
+        $attributes['user_id'] = auth()->id();
+        $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
+
+        Post::create($attributes);
+
+        return redirect('/');
+    }
+
+    public function edit(Post $post)
+    {
+        return view('admin.posts.edit', [
+            'post' => $post
+        ]);
+    }
+}
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,30 +16,6 @@ class PostController extends Controller
         ]);
     }
 
-    public function create()
-    {
-        return view('posts.create');
-    }
-
-    public function store()
-    {
-        $attributes = request()->validate([
-            'title' => ['required'],
-            'thumbnail' => ['required', 'image'],
-            'slug' => ['required', Rule::unique('posts')],
-            'excerpt' => ['required'],
-            'body' => ['required'],
-            'category_id' => ['required', Rule::exists('categories', 'id')]
-        ]);
-
-        $attributes['user_id'] = auth()->id();
-        $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
-
-        Post::create($attributes);
-
-        return redirect('/');
-    }
-
     public function show(Post $post)
     {
         return view('posts.show', [
