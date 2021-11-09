56a53e0b094847b179e9cac873c03489c2c6b00b
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-11-09 10:27:30 +0000

    Show default post values for inputs and textareas

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -3,7 +3,7 @@
 <x-form.field>
     <x-form.label :name="$name" />
 
-    <input class="border border-gray-200 p-2 w-full rounded" type="{{ $type }}" name="{{ $name }}" id="{{ $name }}" value="{{ old($name) }}" {{ $attributes }}>
+    <input class="border border-gray-200 p-2 w-full rounded" type="{{ $type }}" name="{{ $name }}" id="{{ $name }}" {{ $attributes(['value' => old($name)]) }}>
 
     <x-form.error :name="$name" />
 </x-form.field>
